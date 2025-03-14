02c9a157b7f70e936daa632fc29c601a4a4a1ae9
Stella <stella@mbp-de-stella.home>
2025-03-14 10:23:03 +0000

    comments

--- seqClass.py
+++ seqClass.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
 import sys, re
-from argparse import ArgumentParser
+from argparse import ArgumentParser 
 
+# to recieve arguments from the user
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
+# if not, help
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
 args = parser.parse_args()
 
+# convert to upper case 
 args.seq = args.seq.upper()
+
+# check if is dna or rna
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
